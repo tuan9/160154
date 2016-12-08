a2b0d60cbbcc074db5c5d87eb8ad8ce77fedfd99
tuan9 <160154@yses.ac.jp>
2016-12-08 12:04:13 +0900

    AlohaとEchoを作った

--- 20161208/Aloha.java
+++ 20161208/Aloha.java
@@ -1,8 +1,6 @@
 public class Aloha{
 	public static void main(String[] args) {
 		for (int i=0; i<30;i++){
-
-		
 		System.out.println("Aloha!");
 	}
   }
---
+++ 20161208/Echo.java
@@ -0,0 +1,9 @@
+public class Echo{
+	public static void main(String[] args) {
+		String data=new java.util.Scanner(System.in).nextLine();
+		System.out.println(data);
+		if (dta.equals("aoki")) {
+			System.out.println(aoki);
+		}sss
+	}
+}
