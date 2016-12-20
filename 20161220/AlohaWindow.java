20c84f793e675668d1e01299796abb562edf30e5
tuan9 <160154@yses.ac.jp>
2016-12-20 10:49:18 +0900

    AlohaWindow.java done.

---
+++ 20161220/AlohaWindow.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+public class AlohaWindow{
+	public static void main(String[] args) {
+		JFrame frame =new JFrame();
+		frame.setSize(200,200);
+		frame.setVisible(true);
+			
+	}
+  }
