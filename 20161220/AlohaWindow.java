72c3c385742783593de55fa315a8d283e50b1361
tuan9 <160154@yses.ac.jp>
2016-12-20 11:52:00 +0900

    AlohaWindow.java update.

--- 20161220/AlohaWindow.java
+++ 20161220/AlohaWindow.java
@@ -3,8 +3,17 @@ import javax.swing.*;
 public class AlohaWindow{
 	public static void main(String[] args) {
 		JFrame frame =new JFrame();
-		frame.setSize(200,200);
+		frame.setSize(500,200);
+		frame.setTitle("Tuan!");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel label=new JLabel("hello");
+		Container ctnr=frame.getContentPane();
+		ctnr.add(label,BorderLayout.NORTH);
+		JButton button=new JButton("click Me!");
+		ctnr.add(button,BorderLayout.SOUTH);
 		frame.setVisible(true);
+
+
 			
 	}
   }
